e05b703774bb1052cae2252a10ea4e9d713462cd
RagnarKD <ragnar@mail.fo>
2024-10-02 03:25:18 +0100

    default index now displays list of database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProject.Model;
+
 namespace WebProject.Controllers {
     public class HomeController : Controller {
+
+        private DataContext context;
+
+
+        public HomeController(DataContext dbContext) {
+            context = dbContext;
+        }
+
         public IActionResult Index() {
-            return View();
+            return View(context.Translations);
         }
     }
 }
